2c47c09132993c15bf8663c011e74bb85daae017
DungeonMasterNFT <91773840+DungeonMasterNFT@users.noreply.github.com>
2021-10-09 15:34:16 +0700

    Init commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+// Make sure the DevToken contract is included by requireing it.
+const DevToken = artifacts.require("DevToken");
+
+// THis is an async function, it will accept the Deployer account, the network, and eventual accounts.
+module.exports = async function (deployer, network, accounts) {
+};
